ec4489603292288e13ea4db483ab4353f9edb451
potty-dzmeia <ch.levkov@gmail.com>
2015-06-02 18:05:57 +0200

    removed directory cron

--- cron/crontTask_voltageMonitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#voltage=$(./measureVoltage.py >&1)               
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#echo "$DIR"
-$DIR/speakVoltage.sh `$DIR/measureVoltage.py`
--- cron/measureVoltage.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import random
-
-print (float("{0:.2f}".format(random.random()*3)))
